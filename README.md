94428ebfb76c28f7c000937bf2e405109e5cafe5
Albert Still <albertjamesstill@gmail.com>
2019-02-13 16:33:33 +1100

    initial work

---
+++ README.md
@@ -0,0 +1,34 @@
+# only-one
+
+A simple CLI tool that checks you only have one version of a dependency in your
+node_modules folder.
+
+```bash
+only-one react
+```
+
+or for multiple deps
+
+```bash
+only-one react react-dom
+```
+
+If none or more than one version of the deps are found the process will exit
+with failure. The tool works by [globbing](https://en.wikipedia.org/wiki/Glob_(programming) your
+node_modules, it has nothing to do with yarn or npm lock files and therefore checks that yarn or npm
+is also correctly installing into node_modules.
+
+You may wish to add it to your test suite `"test":  "one-dep react && mocha && eslint ."`.
+
+I was motivated to build this because sometimes 3rd party libraries accidentally have
+react as a dependency and this can break stuff.
+
+Please be aware if you have a large node_modules it can be quite slow because it's a lot of file
+paths to search through.
+
+### To installing
+
+```bash
+npm install only-one --save-dev
+yarn add only-one --dev
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+const path = require('path');
+const glob = require('fast-glob');
+
+const deps = process.argv.slice(2);
+
+if (!deps.length) {
+  console.log('No deps passed to only-one, must be like "only-one react react-dom"!');
+  process.exit(1);
+}
+
+// cwd defaults to process.cwd()
+const globPattern = path.join('node_modules', '**', `(${deps.join('|')})`, 'package.json');
+const foundPaths = glob.sync(globPattern);
+
+const resultsMap = new Map();
+
+deps.forEach((dep) => {
+  const foundLocations = foundPaths.filter(packageJsonPath => packageJsonPath.endsWith(path.join(dep, 'package.json')));
+
+  resultsMap.set(dep, foundLocations);
+});
+
+let exitWithFailure = false;
+Array.from(resultsMap.entries()).forEach(([dep, locations]) => {
+  if (locations.length === 1) return;
+
+  if (!exitWithFailure) exitWithFailure = true;
+
+  console.log(
+    `only-one failed for "${dep}" because ${
+      locations.length === 0 ? 'no dep found.' : `${locations.length} deps found at:`
+    }`,
+  );
+  locations.forEach((location) => {
+    console.log(`  - ${location.split(process.cwd())[0].split('package.json')[0]}`);
+  });
+  console.log(); // to cause new line
+});
+
+if (exitWithFailure) {
+  console.log('To debug we suggest using `npm ls` or `yarn why`.');
+  process.exit(1);
+}
+
+console.log(
+  `only-one check passed for ${
+    deps.length === 1
+      ? `"${deps[0]}"`
+      : `"${deps.slice(0, -1).join('", "')}" and "${deps[deps.length - 1]}"`
+  }.`,
+);
+process.exit(0);
